26bfb642307561e68bd4d870a4be6e4ffa31978e
Eugene Kamenskyi <jkamenskiy@gmail.com>
2018-03-11 12:41:19 +0200

    feat(alert.service): provide alertConfig (componentRef and rootElement)
    fix: first destroy bug

--- src/alert.service.ts
+++ src/alert.service.ts
@@ -1,5 +1,5 @@
-import { ApplicationRef, ComponentFactoryResolver, EmbeddedViewRef, Injectable, Injector } from '@angular/core';
-import { IAlertRequest, IAlertResponse } from './alert.interfaces';
+import { ApplicationRef, ComponentFactoryResolver, EmbeddedViewRef, Inject, Injectable, Injector } from '@angular/core';
+import { IAlertConfig, IAlertRequest, IAlertResponse } from './alert.interfaces';
 import { Observable } from 'rxjs/Observable';
 import { AlertComponent } from './alert.component';
 import { ComponentRef } from '@angular/core/src/linker/component_factory';
@@ -7,43 +7,44 @@ import { ComponentRef } from '@angular/core/src/linker/component_factory';
 @Injectable()
 export class AlertService {
 
-    constructor(private componentFactoryResolver: ComponentFactoryResolver,
-                private appRef: ApplicationRef,
-                private injector: Injector) {
+  constructor(private componentFactoryResolver: ComponentFactoryResolver,
+              private appRef: ApplicationRef,
+              @Inject('alertConfig') private alertConfig: IAlertConfig,
+              private injector: Injector) {
+  }
+
+  create(req: IAlertRequest): Observable<IAlertResponse> {
+    if (req.componentRef === undefined || req.componentRef === null) {
+      req.componentRef = this.alertConfig.componentRef;
     }
+    const componentRef = this._appendComponentToAppRoot(req.componentRef);
+    componentRef.instance.config(req);
 
-    create(req: IAlertRequest): Observable<IAlertResponse> {
-        if (req.componentRef === undefined || req.componentRef === null) {
-            req.componentRef = AlertComponent;
-        }
-        const componentRef = this._appendComponentToAppRoot(req.componentRef);
-        componentRef.instance.config(req);
+    // TODO: add disappear animation
+    componentRef.instance.eventResponse
+      .subscribe(e => setTimeout(() => this._destroyComponent(componentRef), 0));
 
-        componentRef.instance.eventResponse.subscribe(e => this._destroyComponent(componentRef));
+    return componentRef.instance.eventResponse;
+  }
 
-        return componentRef.instance.eventResponse;
-    }
+  private _appendComponentToAppRoot(component: typeof AlertComponent): ComponentRef<AlertComponent> {
+    const componentRef = this.componentFactoryResolver
+      .resolveComponentFactory(component)
+      .create(this.injector);
 
-    private _appendComponentToAppRoot(component: typeof AlertComponent): ComponentRef<AlertComponent> {
-        const componentRef = this.componentFactoryResolver
-            .resolveComponentFactory(component)
-            .create(this.injector);
+    this.appRef.attachView(componentRef.hostView);
 
-        this.appRef.attachView(componentRef.hostView);
+    const domElem = (componentRef.hostView as EmbeddedViewRef<any>)
+      .rootNodes[0] as HTMLElement;
 
-        const domElem = (componentRef.hostView as EmbeddedViewRef<any>)
-            .rootNodes[0] as HTMLElement;
+    this.alertConfig.rootElement.appendChild(domElem);
 
-        // Append DOM element to the body
-        document.body.appendChild(domElem);
+    return componentRef;
+  }
 
-        return componentRef;
-    }
-
-    private _destroyComponent(componentRef: ComponentRef<AlertComponent>): void {
-        this.appRef.detachView(componentRef.hostView);
-        componentRef.destroy();
-    }
-
+  private _destroyComponent(componentRef: ComponentRef<AlertComponent>): void {
+    this.appRef.detachView(componentRef.hostView);
+    componentRef.destroy();
+  }
 
 }
