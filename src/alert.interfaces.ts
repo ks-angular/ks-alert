a5ca43d198e847261d02062d54a314bfe257e974
Eugene Kamenskyi <jkamenskiy@gmail.com>
2018-03-11 12:36:57 +0200

    feat(alert.interfaces): add IAlertConfig
    rename AlertType to IconType
    add isConfirm to IAlertButton

--- src/alert.interfaces.ts
+++ src/alert.interfaces.ts
@@ -1,53 +1,71 @@
-import { AlertComponent } from './alert.component';
 import { TemplateRef } from '@angular/core';
+import { AlertComponent } from './alert.component';
 
-export enum AlertType {
-    NONE = 0,
-    SUCCESS = 1,
-    WARNING,
-    DANGER,
-    INFO,
+export enum IconType {
+  NONE = 0,
+  SUCCESS = 1,
+  WARNING,
+  DANGER,
+  INFO,
 }
 
 export enum AlertButtonType {
-    NONE = 0,
-    YES = 1,
-    OK,
-    RETRY,
-    NO,
-    ABORT,
-    CANCEL,
-    IGNORE,
-    CONFIRM,
-    ALLOW,
-    DENY
-}
-
-export interface IAlertOptions {
-    overlay?: boolean;
-    overlayClickToCancel?: boolean;
-    showCloseButton?: boolean;
-    duration?: number; // after time out alert will closed with CANCEL trigger
+  NONE = 0,
+  YES = 1,
+  OK,
+  RETRY,
+  NO,
+  ABORT,
+  CANCEL,
+  IGNORE,
+  CONFIRM,
+  ALLOW,
+  DENY
 }
 
 export class IAlertButton {
-    title?: string;
-    classes?: string; // css class
-    type: AlertButtonType; // approximately type
+  title?: string;
+  classes?: string; // css class
+  isConfirm: boolean;
+  type?: AlertButtonType;
+}
+
+export class IAlertIcon {
+  src?: string;
+  classes?: string;
+}
+
+export interface IAlertOptions {
+  overlay?: boolean;
+  overlayClickToCancel?: boolean;
+  showCloseButton?: boolean;
+  duration?: number; // after time out the alert will be closed with CANCEL trigger
 }
 
 export interface IAlertRequest {
-    caption?: string;
-    text?: string;
-    alertType?: AlertType;
-    alertButtons?: (AlertButtonType | IAlertButton) [];
-    options?: IAlertOptions;
-    componentRef?: typeof AlertComponent;
-    templateRef?: TemplateRef<any>;
+  caption?: string;
+  text?: string;
+  iconType?: IconType | IAlertIcon;
+  alertButtons?: (AlertButtonType | IAlertButton) [];
+  options?: IAlertOptions;
+  componentRef?: typeof AlertComponent;
+  templateRef?: TemplateRef<any>;
 }
 
 export class IAlertResponse {
-    request: IAlertRequest;
-    responseButton: IAlertButton;
-    confirm: boolean; // Equal to: CONFIRM, YES, OK, RETRY, ALLOW
+  request: IAlertRequest;
+  responseButton: IAlertButton;
+  confirm: boolean;
+}
+
+export class IAlertConfig {
+  caption?: string;
+  text?: string;
+  iconType?: IconType | IAlertIcon;
+  alertButtons?: (AlertButtonType | IAlertButton) [];
+  options?: IAlertOptions;
+  componentRef?: typeof AlertComponent;
+  templateRef?: TemplateRef<any>;
+  alertButtonsResource?: IAlertButton[]; // sequential are the same as AlertButtonType enum
+  rootElement?: any; // set parent of alertComponent
 }
